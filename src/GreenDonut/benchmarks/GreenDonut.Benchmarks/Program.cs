ee36173194dddfdadc43ee0f29044942d0a1c0de
Viktor Faddi <faddiv@gmail.com>
2024-09-23 16:51:47 +0200

    Add benchmark from my branch

---
+++ src/GreenDonut/benchmarks/GreenDonut.Benchmarks/Program.cs
@@ -0,0 +1,17 @@
+// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using GreenDonut.Benchmarks;
+
+Console.WriteLine("Hello, World!");
+/*
+var v = new MultiThreadPerformanceBenchmarks();
+v.Setup();
+var result = await v.UncachedLoad();
+var result2 = await v.CachedLoad();
+*/
+
+BenchmarkRunner.Run(
+    [
+        typeof(SingleThreadPerformanceBenchmarks),
+        typeof(MultiThreadPerformanceBenchmarks)
+    ]);
---
+++ src/GreenDonut/benchmarks/GreenDonut.Benchmarks/TestInfrastructure/ManualBatchScheduler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+
+namespace GreenDonut.Benchmarks.TestInfrastructure;
+
+public class ManualBatchScheduler : IBatchScheduler
+{
+    private readonly ConcurrentQueue<Func<ValueTask>> _queue = new();
+
+    public void Dispatch()
+    {
+        while (_queue.TryDequeue(out var dispatch))
+        {
+            Task.Run(async () => await dispatch());
+        }
+    }
+
+    public Task DispatchAsync()
+    {
+        var tasks = new List<Task>();
+        while (_queue.TryDequeue(out var dispatch))
+        {
+
+            tasks.Add(Task.Run(async () => await dispatch()));
+        }
+        return tasks.Count > 0
+            ? Task.WhenAll(tasks)
+            : Task.CompletedTask;
+    }
+
+    public void Schedule(Func<ValueTask> dispatch)
+    {
+        _queue.Enqueue(dispatch);
+    }
+}
